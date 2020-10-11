358c6a6f2aa544c493c26ad6ee6462c8afdfd6b6
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-10-11 22:57:19 +0200

    Refactor HTTP Server to use Echo

--- hack/server.go
+++ hack/server.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/Octops/agones-relay-http/internal/runtime"
 	"github.com/Octops/agones-relay-http/pkg/broker"
-	"io/ioutil"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"log"
 	"net/http"
 	"strings"
@@ -19,40 +18,43 @@ curl --header "Content-Type: application/json" \
   http://localhost:8090
 */
 
+var addr = ":8090"
+
 func main() {
-	server := &http.Server{Addr: ":8090"}
+	e := echo.New()
+	//e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodDelete {
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				log.Printf("Error reading body: %v", err)
-				http.Error(w, "can't read body", http.StatusBadRequest)
-				return
-			}
-
-			var payload broker.Payload
-			err = json.Unmarshal(body, &payload)
-			if err != nil {
-				log.Printf("%s - %s", body, err.Error())
-				fmt.Fprintf(w, "%s", body)
-				return
-			}
-			log.Printf("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
-			fmt.Fprintf(w, "%s", payload.Body.Header.Headers)
-		} else {
-
-			log.Printf("webhook received: ondelete/%s %s-%s", r.URL.Query()["source"], r.URL.Query()["namespace"], r.URL.Query()["name"])
-			fmt.Fprintf(w, "%s", "DELETED")
+	e.POST("/webhook", func(c echo.Context) error {
+		var payload broker.Payload
+		if err := c.Bind(&payload); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
 		}
 
+		log.Printf("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
+		return c.String(http.StatusCreated, "OK")
 	})
 
-	log.Println("server listening at", server.Addr)
+	e.PUT("/webhook", func(c echo.Context) error {
+		var payload broker.Payload
+		if err := c.Bind(&payload); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+		}
+
+		log.Printf("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
+		return c.String(http.StatusOK, "OK")
+	})
+
+	e.DELETE("/webhook", func(c echo.Context) error {
+		log.Printf("webhook received: ondelete/%s %s-%s", c.QueryParam("source"), c.QueryParam("namespace"), c.QueryParam("name"))
+		return c.String(http.StatusOK, "OK")
+	})
+
+	log.Println("server listening at", addr)
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			panic(err)
+		if err := e.Start(addr); err != nil {
+			e.Logger.Info("shutting down the server")
 		}
 	}()
 
@@ -61,9 +63,10 @@ func main() {
 
 	<-ctx.Done()
 
-	log.Println("stopping server")
+	defer cancel()
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Print(err)
+	log.Println("stopping server")
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
 	}
 }
