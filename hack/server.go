23f2c1e3cfb0b565fc3f8f645f7aad6e759b4741
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-10-12 15:15:44 +0200

    Improve local dev server with better logging

--- hack/server.go
+++ hack/server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"github.com/Octops/agones-relay-http/internal/runtime"
 	"github.com/Octops/agones-relay-http/pkg/broker"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"log"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strings"
 )
@@ -18,39 +20,82 @@ curl --header "Content-Type: application/json" \
   http://localhost:8090
 */
 
-var addr = ":8090"
+var (
+	addr    string
+	verbose bool
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", ":8090", "address for the server to be listening")
+	flag.BoolVar(&verbose, "verbose", true, "show verbose logs")
+}
+
+type Logger func(c echo.Context) error
+
+func logVerbose(c echo.Context) error {
+	var payload broker.Payload
+	if err := c.Bind(&payload); err != nil {
+		logrus.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+	}
+
+	p, err := json.Marshal(&payload)
+	if err != nil {
+		logrus.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+	}
+
+	logrus.Infof("webhook received: %s/%s %s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]), p)
+	return nil
+}
+
+func logAcknowledge(c echo.Context) error {
+	var payload broker.Payload
+	if err := c.Bind(&payload); err != nil {
+		logrus.Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+	}
+
+	logrus.Infof("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
+	return nil
+}
 
 func main() {
+	flag.Parse()
+
 	e := echo.New()
-	//e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	var logger Logger = logAcknowledge
+
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	if verbose {
+		e.Use(middleware.Logger())
+		logrus.SetLevel(logrus.DebugLevel)
+		logger = logVerbose
+	}
+
 	e.POST("/webhook", func(c echo.Context) error {
-		var payload broker.Payload
-		if err := c.Bind(&payload); err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+		if err := logger(c); err != nil {
+			return err
 		}
 
-		log.Printf("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
-		return c.String(http.StatusCreated, "OK")
+		return c.String(http.StatusOK, "OK")
 	})
 
 	e.PUT("/webhook", func(c echo.Context) error {
-		var payload broker.Payload
-		if err := c.Bind(&payload); err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "request body does not contain a valid payload")
+		if err := logger(c); err != nil {
+			return err
 		}
 
-		log.Printf("webhook received: %s/%s", strings.ToLower(payload.Body.Header.Headers["event_source"]), strings.ToLower(payload.Body.Header.Headers["event_type"]))
 		return c.String(http.StatusOK, "OK")
 	})
 
 	e.DELETE("/webhook", func(c echo.Context) error {
-		log.Printf("webhook received: ondelete/%s %s-%s", c.QueryParam("source"), c.QueryParam("namespace"), c.QueryParam("name"))
+		logrus.Infof("webhook received: ondelete/%s %s-%s", c.QueryParam("event_type"), c.QueryParam("namespace"), c.QueryParam("name"))
 		return c.String(http.StatusOK, "OK")
 	})
-
-	log.Println("server listening at", addr)
 
 	go func() {
 		if err := e.Start(addr); err != nil {
@@ -65,7 +110,7 @@ func main() {
 
 	defer cancel()
 
-	log.Println("stopping server")
+	logrus.Info("stopping server")
 	if err := e.Shutdown(ctx); err != nil {
 		e.Logger.Fatal(err)
 	}
